8dbb9502461217d1379e896fa713e5c8e2cf64e1
Suchit007-sk <kawadesuchit@gmail.com>
2024-12-30 19:44:39 +0530

    New DataTypes Added

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -1,3 +1,44 @@
  "use strict"; // treat all code as newer version
  //alert(3+3) we are using node js not browser
+ // number datatype
+ var x = 10.99
+ console.log(typeof x)
+
+ // bigInt datatype
+ var y = 10n
+ console.log(typeof y)
+
+ //string
+ var nam = "Suchit"
+ console.log(typeof nam);
+
+ //boolean
+ var isMonday = true
+ console.log(typeof isMonday)
+
+ //undefined
+ var z
+ console.log(typeof z)
+
+ //objects
+ var obj = {}
+ console.log(typeof obj);
+
+ var arr = []
+ console.log(typeof arr);
+
+ // Symbol- It is used as unique identifier
+ var sym = Symbol("id")
+ console.log(typeof sym)
+
+ // null
+ var x = 10
+ x=null
+ console.log(typeof x);
+ 
+
+
+
+ 
+ 
  
