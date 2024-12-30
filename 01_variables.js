67a3794d2074b75e1d893c2dd4cbe23be7239899
Suchit007-sk <kawadesuchit@gmail.com>
2024-12-30 13:19:32 +0530

    chane

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+/* var Keyword Whole Detail*/
+
+var names ="Suchit"
+console.log(names)
+names="Suchit Kawade"
+console.log(names)
+var names = "Suchit Balasaheb Kawade"
+console.log(names)
+
+/*--var Keyword Whole Detail--*/
