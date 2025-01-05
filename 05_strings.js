3145d7fd6f023c531ccfc55d95c7f913551705d7
Suchit007-sk <kawadesuchit@gmail.com>
2025-01-05 15:17:16 +0530

    Strings Added

---
+++ 05_strings.js
@@ -0,0 +1,16 @@
+/*const nert = "Suchit"
+const repaCert = "the Don"
+console.log(`My Name is ${nert} and I am ${repaCert}`);
+const gameName = new String('SupriyaKawade')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());*/
+const gameNames = new String('SupriyaKawade')
+//const news = gameNames.substring(0,4)
+//console.log(news);
+
+const newst = gameNames.slice(0,4)
+console.log(newst);
+const newste = gameNames.slice(-11,4)
+console.log(newste);
