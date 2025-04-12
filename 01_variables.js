105db9cf836ebfb9d9c019fc864571574ea38833
Suchit007-sk <kawadesuchit@gmail.com>
2025-04-12 19:41:06 +0530

    nums Pushed

--- 01_variables.js
+++ 01_variables.js
@@ -1,7 +1,17 @@
-const accountID = 123456;
+/*const accountID = 123456;
 let  accountEmail = "suchitkawade@gmail.com";
 var accountPassword = "123456";
 accountCity = "Pune";
 
 //accountID = 2
-console.log(accountID);
+console.log(accountID);*/
+
+function filterDropdown() {
+    const input = document.getElementById('ddlSearch').value.toLowerCase();
+    const options = document.getElementById('ddlClasses').options;
+    
+    for (let i = 0; i < options.length; i++) {
+      const text = options[i].text.toLowerCase();
+      options[i].style.display = text.includes(input) ? '' : 'none';
+    }
+  }
