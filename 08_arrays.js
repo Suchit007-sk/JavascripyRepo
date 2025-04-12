bf28c8a4759b6df9ecf21b0f9bf08fe6fb5aadb5
Suchit007-sk <kawadesuchit@gmail.com>
2025-04-13 01:23:05 +0530

    arrays added

--- 08_arrays.js
+++ 08_arrays.js
@@ -1,19 +1,27 @@
-let myArr = [1,2,3,4,5]
+// let myArr = [1,2,3,4,5]
 
-// Slice 
+// // Slice 
 
-console.log("A", myArr);
+// console.log("A", myArr);
 
-const myNewarr = myArr.slice(1,3)
+// const myNewarr = myArr.slice(1,3)
 
-console.log(myNewarr);
-console.log("B", myArr);
+// console.log(myNewarr);
+// console.log("B", myArr);
 
-// Splice
-const myNewarr1 = myArr.splice(1,3)
+// // Splice
+// const myNewarr1 = myArr.splice(1,3)
 
-console.log(myNewarr1);
-console.log("c", myArr);
+// console.log(myNewarr1);
+// console.log("c", myArr);
+
+const myArray = ["Superman","Shaktiman","Thor"]
+
+ myArray.push("Ironman")
+
+console.log(myArray);
+
+
 
 
 
