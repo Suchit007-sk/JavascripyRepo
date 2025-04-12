d88d6ae35880108fb3ca6b9aa48c7d916000f40f
Suchit007-sk <kawadesuchit@gmail.com>
2025-04-12 20:24:28 +0530

    slice and Splice learned

---
+++ 08_arrays.js
@@ -0,0 +1,20 @@
+let myArr = [1,2,3,4,5]
+
+// Slice 
+
+console.log("A", myArr);
+
+const myNewarr = myArr.slice(1,3)
+
+console.log(myNewarr);
+console.log("B", myArr);
+
+// Splice
+const myNewarr1 = myArr.splice(1,3)
+
+console.log(myNewarr1);
+console.log("c", myArr);
+
+
+
+
