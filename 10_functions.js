801b57c3f18a4435265c923e824a7dede07e51a4
Suchit007-sk <kawadesuchit@gmail.com>
2025-04-14 01:59:17 +0530

    functions added

---
+++ 10_functions.js
@@ -0,0 +1,14 @@
+// Declaration Of Function
+function sayMyName() {
+    console.log("Suchit");
+    console.log("Kawade");
+    console.log("Balasaheb");
+    
+}
+
+// Reference Of Function
+//sayMyName;
+
+// Execution Of Function
+
+sayMyName();
